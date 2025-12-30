64b1ef8560794bb840720b158f9b129817cb9ded
jibeparent-droid <>
2025-12-30 11:50:18 +0000

    Tuple Basics

--- mastering_python_tuples/tuple_basics/main.py
+++ mastering_python_tuples/tuple_basics/main.py
@@ -1,5 +1,5 @@
 # Write your code here
-space_movies = ___
+space_movies =('2001: A Space Odyssey', 'Interstellar', 'Star Wars: Episode IV - A New Hope', 'Gravity', 'The Martian')
+space_movies = tuple(space_movies)
 
-# Testing
 print("Movies about space:", space_movies)
