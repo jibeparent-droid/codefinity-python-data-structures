8fb43b8193835e8e20afc2a204beaa9ceb3b75dc
jibeparent-droid <>
2025-12-31 13:20:56 +0000

    Tuple modification

---
+++ mastering_python_tuples/tuple_modification/main.py
@@ -0,0 +1,14 @@
+movie_genres = ("Action", "Comedy", "Drama", "Horror", "Sci-Fi")
+
+# Write your code here
+temp_list = [___]
+
+temp_list[___] = "Thriller"  
+temp_list[___] = "Adventure"  
+
+movie_genres = ___
+
+___
+
+# Testing
+print("Updated genres:", movie_genres)
